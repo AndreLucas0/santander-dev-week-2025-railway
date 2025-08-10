0c5e76cfc3a906695806416a463874b0bf6e81ee
Andre Lucas Ferreira <alucas3103@outlook.com>
2025-08-10 18:46:19 -0300

    first commit

---
+++ src/main/java/com/andre/santander_dev_week_api_railway/SantanderDevWeekApiRailwayApplication.java
@@ -0,0 +1,13 @@
+package com.andre.santander_dev_week_api_railway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderDevWeekApiRailwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderDevWeekApiRailwayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andre/santander_dev_week_api_railway/SantanderDevWeekApiRailwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andre.santander_dev_week_api_railway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderDevWeekApiRailwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
